fef034c5ecef289d4160ad69e198adf261bd4a0b
anisafatmawati <fatmawatianisa10@gmail.com>
2020-05-09 23:20:00 +0700

    bab 1

---
+++ app/src/main/java/com/example/challenge2/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.challenge2
+
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter (fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    private val JUMLAH_MENU = 3
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> { return MyFriendFragment() }
+            1 -> { return GithubFragment() }
+            2 -> { return ProfileFragment() }
+            else -> {
+                return GithubFragment()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return JUMLAH_MENU
+    }
+
+}
